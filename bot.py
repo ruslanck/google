85818a10178d270217b36e247f349dac65186c04
rus-coder <ruslanselkovnikov208@gmail.com>
2026-04-10 21:47:23 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import telebot
+
+
+bot = telebot.TeleBot("7863973073:AAEKZiqaMS9PnEj5ygqqwqdgXFW3YK_PSXk")
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.reply_to(message, "этот бот повторяет ваши сообшения как эхо")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
